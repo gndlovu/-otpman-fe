5ec2c3334a01ee8bec2c395fec5691991f65d54e
Gladwell Ndlovu <gladwell_n@live.com>
2021-11-14 22:47:15 +0200

    Build assets

--- lib/data/services/otp_service.dart
+++ lib/data/services/otp_service.dart
@@ -3,7 +3,9 @@ import 'package:dartz/dartz.dart';
 import 'package:otpman_fe/core/error/failures.dart';
 
 class OtpService {
-  final baseUrl = 'http://localhost:2000/api/v1/auth/otp';
+  // final baseUrl = 'http://localhost:2000/api/v1/auth/otp';
+  final baseUrl =
+      'http://gladwell-ndlovu.php.test.entrostat.xyz:8080/api/v1/auth/otp';
 
   Future<Either<Failure, String>> requestOtp(String email) async {
     try {
